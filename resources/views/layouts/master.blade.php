79a6b877fd1956856c0ab9539568af5dd1c5887d
Andrey Sabile <sabileandrey@gmail.com>
2020-10-01 11:48:47 +0800

    added home.css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ asset('/css/tailwind.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
         <title>Filaus</title>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Source+Serif+Pro:wght@700&display=swap" rel="stylesheet">
         @stack('scripts')
